86efc804eb9c92d8526abd128951560a938aca12
Mitch Lillie <mitch@mitchlillie.com>
2018-01-04 07:13:24 -0800

    update sha

--- git-hooks.rb
+++ git-hooks.rb
@@ -2,7 +2,7 @@ class GitHooks < Formula
   desc "A A tool to manage project, user, and global Git hooks"
   homepage "https://github.com/icefox/git-hooks"
   url "https://github.com/MitchLillie/homebrew-git-hooks/raw/master/archive/git-hooks-1.0.0.tar.gz"
-  sha256 "f8dead0776f3753e2c9ece1d62e86348be4f46bc1c724f7b6b481145a3530e83"
+  sha256 "936a67bb6d9352a4869a8c8ab66610e5b3eeccec80db3384d8f40abdc499c555"
   version "1.0.0"
 
   bottle :unneeded
